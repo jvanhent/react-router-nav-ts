1aea6fc56e6cca08495aa6b053f82e56262461e3
Johnny Vanhentenryk <johnny.vanhentenryk@ixor.be>
2023-02-28 00:48:43 +0100

    Fix github pages path issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles.css"
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter basename="/react-router-nav-ts">
+      <HashRouter >
           <App />
-      </BrowserRouter>
+      </HashRouter>
 
   </React.StrictMode>
 );
